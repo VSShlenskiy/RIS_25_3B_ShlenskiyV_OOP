c4b6510a6a3662157f45061ace64b237b1158851
VSShlenskiy <slenskijvitalij199@gmail.com>
2026-09-12 10:51:44 +0500

    fix tipes

--- 1 laba/1_lab/Program.cs
+++ 1 laba/1_lab/Program.cs
@@ -15,7 +15,7 @@ namespace Lab1
                 Console.WriteLine("Exit - 0");
                 Console.Write("Enter number: ");
 
-                var q = 0;
+                int q = 0;
                 while (!int.TryParse(Console.ReadLine(), out q))
                 {
                     Console.Write("Invalid input! Enter number: ");
@@ -51,29 +51,29 @@ namespace Lab1
         public void Run()
         {
             Console.Write("n? ");
-            var firstNumber = 0;
+            int firstNumber = 0;
             while (!int.TryParse(Console.ReadLine(), out firstNumber))
             {
                 Console.Write("Error! Please re-enter n: ");
             }
 
             Console.Write("m? ");
-            var secondNumber = 0;
+            int secondNumber = 0;
             while (!int.TryParse(Console.ReadLine(), out secondNumber))
             {
                 Console.Write("Error! Please re-enter m: ");
             }
 
-            var firstNumberCopy = firstNumber;
-            var result1 = ProductWithPostIncrement(firstNumberCopy, secondNumber);
+            int firstNumberCopy = firstNumber;
+            int result1 = ProductWithPostIncrement(firstNumberCopy, secondNumber);
             Console.WriteLine($"n={firstNumberCopy}  m={secondNumber}  n++*m = {result1}");
 
-            var firstNumberCopy2 = firstNumber;
-            var result2 = LessThanWithPostIncrement(firstNumberCopy2, secondNumber);
+            int firstNumberCopy2 = firstNumber;
+            bool result2 = LessThanWithPostIncrement(firstNumberCopy2, secondNumber);
             Console.WriteLine($"n={firstNumberCopy2}  m={secondNumber}  n++<m = {result2}");
 
-            var secondNumberCopy = secondNumber;
-            var result3 = GreaterThanWithPreDecrement(firstNumber, secondNumberCopy);
+            int secondNumberCopy = secondNumber;
+            bool result3 = GreaterThanWithPreDecrement(firstNumber, secondNumberCopy);
             Console.WriteLine($"m={secondNumberCopy}  n={firstNumber}  --m>n = {result3}");
 
             for (int x = -10; x < 10; x++)
@@ -115,20 +115,20 @@ namespace Lab1
         public void Run() 
         {
             Console.WriteLine("Enter value pointX: ");
-            var pointX = 0d;
+            double pointX = 0d;
             while (!double.TryParse(Console.ReadLine(), out pointX))
             {
                 Console.Write("Error! Please re-enter the number: ");
             }
 
             Console.WriteLine("Enter value pointY: ");
-            var pointY = 0d;
+            double pointY = 0d;
             while (!double.TryParse(Console.ReadLine(), out pointY))
             {
                 Console.Write("Error! Please re-enter the number: ");
             }
 
-            var isInArea = IsPointInArea(pointX, pointY);
+            bool isInArea = IsPointInArea(pointX, pointY);
             Console.WriteLine(isInArea ? "The point belongs to the shaded area." : "The point does not belong to the shaded area.");
         }
 
@@ -147,44 +147,44 @@ namespace Lab1
     {
         public void Run()
         {
-            var a1 = 1000f;
-            var b1 = 0.0001f;
+            float a1 = 1000f;
+            float b1 = 0.0001f;
             Console.WriteLine("float = " + CalculateFloat(a1, b1));
 
-            var a2 = 1000;
-            var b2 = 0.0001;
+            double a2 = 1000;
+            double b2 = 0.0001;
             Console.WriteLine("double = " + CalculateDouble(a2, b2));
         }
 
         private float CalculateFloat(float a1, float b1)
         {
-            var diff = a1 - b1;
-            var diffCubed = diff * diff * diff;
-            var aCubed = a1 * a1 * a1;
-            var numerator = diffCubed - aCubed;
+            float diff = a1 - b1;
+            float diffCubed = diff * diff * diff;
+            float aCubed = a1 * a1 * a1;
+            float numerator = diffCubed - aCubed;
 
-            var bSquared = b1 * b1;
-            var bCubed = b1 * b1 * b1;
-            var aSquared = a1 * a1;
-            var denominator = 3 * a1 * bSquared - bCubed - 3 * aSquared * b1;
+            float bSquared = b1 * b1;
+            float bCubed = b1 * b1 * b1;
+            float aSquared = a1 * a1;
+            float denominator = 3 * a1 * bSquared - bCubed - 3 * aSquared * b1;
 
-            var result = numerator / denominator;
+            float result = numerator / denominator;
             return result;
         }
 
         private double CalculateDouble(double a2, double b2)
         {
-            var diff = a2 - b2;
-            var diffCubed = Math.Pow(diff, 3);
-            var aCubed = Math.Pow(a2, 3);
-            var numerator = diffCubed - aCubed;
+            double diff = a2 - b2;
+            double diffCubed = Math.Pow(diff, 3);
+            double aCubed = Math.Pow(a2, 3);
+            double numerator = diffCubed - aCubed;
 
-            var bSquared = Math.Pow(b2, 2);
-            var bCubed = Math.Pow(b2, 3);
-            var aSquared = Math.Pow(a2, 2);
-            var denominator = 3 * a2 * bSquared - bCubed - 3 * aSquared * b2;
+            double bSquared = Math.Pow(b2, 2);
+            double bCubed = Math.Pow(b2, 3);
+            double aSquared = Math.Pow(a2, 2);
+            double denominator = 3 * a2 * bSquared - bCubed - 3 * aSquared * b2;
 
-            var result = numerator / denominator;
+            double result = numerator / denominator;
             return result;
         }
     }
